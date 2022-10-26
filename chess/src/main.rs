01465c88549088a058e21775290b6241330e85ee
dgarc359 <dgarc359@outlook.com>
2022-10-26 01:24:05 -0400

    skeleton for chess game

---
+++ chess/src/main.rs
@@ -0,0 +1,23 @@
+enum Pieces {
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King
+}
+
+enum Team {
+    White,
+    Black
+}
+
+struct Piece {
+    legal_moves: Vec<u16>,
+    kind: Pieces,
+    team: Team,
+}
+
+fn main() {
+    let mut board: u16;
+}
