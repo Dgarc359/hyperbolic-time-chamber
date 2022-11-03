c894cadf253a7ce9d235d35e96c5f3342cece8dd
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2022-11-03 17:07:22 -0400

    it worksgit add .

--- chess/src/board.rs
+++ chess/src/board.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
 
-type myMap<'a> = HashMap<u16, &'a u8>;
+type MyMap<'a> = HashMap<u16, &'a u8>;
 
 pub struct Board<'a> {
-  pub position: myMap<'a>
+  pub position: MyMap<'a>
 }
 
-fn build_board() -> myMap<'static> {
-  let mut map: myMap = HashMap::new();
+fn build_board() -> MyMap<'static> {
+  let mut map: MyMap = HashMap::new();
 
   map.insert(0, &7);
   map
@@ -15,14 +15,14 @@ fn build_board() -> myMap<'static> {
 
 impl Board<'_> {
   pub fn new() -> Self {
-    let pos: myMap = build_board();
+    let pos: MyMap = build_board();
     Board {
       position: pos
     }
   }
 
   pub fn move_piece(&mut self, from: u16, to: u16){
-    let piece = self.position.get(&from).unwrap();
+    let piece = self.position.get(&from).unwrap().to_owned();
     self.position.remove(&from);
     self.position.insert(to, piece);
   }
--- chess/src/main.rs
+++ chess/src/main.rs
@@ -7,5 +7,8 @@ mod board;
 pub use board::{Board};
 
 fn main() {
-    let board = Board::new();
+    let mut board = Board::new();
+    board.move_piece(0, 1);
+    let new_piece = board.position.get(&1).unwrap().to_owned();
+    println!("{:?}", new_piece);
 }
